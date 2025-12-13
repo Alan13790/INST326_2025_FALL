22e52b828a2ea40d34c549b005c6f71090f603ca
Alan13790 <alan1379@terpmail.umd.edu>
2025-12-12 19:19:05 -0500

    Update game_end.py

--- game_end.py
+++ game_end.py
@@ -1,9 +1,6 @@
 def check_game_end(self):
        """Return True if no assets and cannot afford a crop.
 
-
-       Techniques used:
-       - set operations (union)
        """
        reasons = set()
        if not (self.crops or self.animals):
