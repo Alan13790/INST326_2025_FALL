dff5cd2b83d7e9b67e12e2830c1fde204b4279b2
Alan13790 <alan1379@terpmail.umd.edu>
2025-12-12 19:19:27 -0500

    Update final_score.py

--- final_score.py
+++ final_score.py
@@ -1,9 +1,6 @@
 def final_score(self):
        """Calculate and print final score.
 
-
-       Techniques used:
-       - f-strings containing expressions
        """
        remaining = sum(r.sell_price for r in self.crops + self.animals)
        score = self.money + remaining + self.level * 100
